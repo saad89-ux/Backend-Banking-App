c386fff70d975f2d1d25d38c56094dbd7892b076
saadkamran <saadkamran874@gmail.com>
2026-03-03 17:58:00 +0500

    correct

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:8080", "http://localhost:3000"],
+  origin: ["https://bankwise-connect.vercel.app/", "http://localhost:3000"],
   credentials: true,
 }));
 
